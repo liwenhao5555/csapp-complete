deab31dbdc94d80879ad0443635fda20da8c81f8
liwenhao5555 <liwenhao5555@hotmail.com>
2024-06-17 19:17:57 +0800

    try cygwin compilation

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Computer Systems: A Programmer's Perspective, 3/E (CS:APP3e)
 https://csapp.cs.cmu.edu/3e/code.html
 
 
-Environment:
+Environment(Legacy):
 Aliyun ECS ecs.t6-c1m2.large 2vCPU 4GiB
 ubuntu_22_04_x64_20G_alibase_20240322.vhd
+
+I use Cygwin for self-study.
--- intro/hello.s
+++ intro/hello.s
@@ -1,41 +1,23 @@
 	.file	"hello.c"
 	.text
-	.section	.rodata.str1.1,"aMS",@progbits,1
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
 .LC0:
-	.string	"hello, world"
+	.ascii "hello, world\0"
 	.text
 	.globl	main
-	.type	main, @function
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
 main:
-.LFB23:
-	.cfi_startproc
-	endbr64
-	subq	$8, %rsp
-	.cfi_def_cfa_offset 16
-	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
+	subq	$40, %rsp
+	.seh_stackalloc	40
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rcx
+	call	puts
 	movl	$0, %eax
-	addq	$8, %rsp
-	.cfi_def_cfa_offset 8
+	addq	$40, %rsp
 	ret
-	.cfi_endproc
-.LFE23:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
+	.seh_endproc
+	.ident	"GCC: (GNU) 11.4.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
